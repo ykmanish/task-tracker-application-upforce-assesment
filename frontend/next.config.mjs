e07c6cca0d3957dc63ba7e8112125b42e67a6552
Manishkumar Yadav <106073247+ykmanish@users.noreply.github.com>
2024-10-27 22:47:32 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Enable static export
+    output: 'export',
+  };
+  
+  export default nextConfig;
+  
