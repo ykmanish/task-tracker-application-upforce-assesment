3f3e35980f56bbabff1a5f61d44da3907dcaf8be
Manish <my79220@gmail.com>
2024-10-28 00:18:20 +0530

    Completion of the Project

--- frontend/src/components/TaskDashboard/SearchBar.jsx
+++ frontend/src/components/TaskDashboard/SearchBar.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
   return (
-    <div className=" flex justify-center items-center w-full">
-      <div className="relative inset-y-0 lg:left-8  left-3 start-0 flex items-center  pointer-events-none">
-        <img src="/search.svg" alt="search" className="w-4 h-4" />
+    <div className="flex justify-center items-center w-full">
+      <div className="relative w-full max-w-lg"> {/* Set max-width for larger screens */}
+        <input
+          type="text"
+          className="bg-white border placeholder:text-zinc-500 input input-bordered py-3 border-gray-300 text-gray-900 text-sm rounded-xl block pl-10 pr-3 w-full" 
+          placeholder="Search here..."
+          required
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <img
+          src="/search.svg"
+          alt="search"
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 pointer-events-none"
+        />
       </div>
-      <input
-        type="text"
-        className="bg-white lg:w-80   border placeholder:text-zinc-500 small input input-bordered py-3 border-gray-300 text-gray-900 text-sm rounded-xl block ps-10 p-2.5"
-        placeholder="Search here..."
-        required
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
     </div>
   );
 };
